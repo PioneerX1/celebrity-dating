a816e295b3507224d557f8a6b6f48342f5f8170d
Alena Golovina and Mick Sexton <mick@>
2017-06-01 16:28:18 -0700

    added js:conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  $("#user-input").submit(function() {
+    var inputAge = parseInt($("input#age").val());
+    var inputGender = $("select#gender").val();
+
+    $('.results').show();
+    if (inputAge < 18 && inputGender === "male") {
+        $("#celebrity").text("Madonna");
+    }
+    if (inputAge < 18 && inputGender === "female") {
+      $("#celebrity").text("Minnie");
+    }
+    if (inputAge >= 18 && inputGender === "male") {
+      $("#celebrity").text("Mickey");
+    }
+    if (inputAge >= 18 && inputGender === "female") {
+      $("#celebrity").text("Mamma");
+    }
+    event.preventDefault();
+  });
 });
